9a492c30fd2a57697c38d30c87652fce972793ce
Freddy Chiron <freddy.chiron@gmail.com>
2018-02-07 15:56:07 +0100

    [KATA] add java8 style computation

--- src/main/java/rpn/ReversePolishNotation.java
+++ src/main/java/rpn/ReversePolishNotation.java
@@ -2,11 +2,14 @@ package rpn;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Stack;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 
 public class ReversePolishNotation {
 
 	private static final String SEPARATOR = " ";
-	
+
 	public static BigDecimal compute(String expression) {
 
 		if (expression.isEmpty()) {
@@ -19,8 +22,6 @@ public class ReversePolishNotation {
 			return new BigDecimal(expression);
 		}
 
-		
-		
 		String firstOperator = expressionElements[2];
 		BigDecimal firstResult = getFirstResult(firstOperator, new BigDecimal(expressionElements[0]),
 				new BigDecimal(expressionElements[1]));
@@ -29,7 +30,7 @@ public class ReversePolishNotation {
 	}
 
 	private static BigDecimal getFirstResult(String firstOperator, BigDecimal firstOperand, BigDecimal secondOperand) {
-		System.out.println("firstOperand="+firstOperand+" secondOperand="+secondOperand);
+		System.out.println("firstOperand=" + firstOperand + " secondOperand=" + secondOperand);
 		switch (firstOperator) {
 		case "+":
 			return firstOperand.add(secondOperand);
@@ -43,4 +44,39 @@ public class ReversePolishNotation {
 			return new BigDecimal("0");
 		}
 	}
+
+	public static BigDecimal computeUsingJava8Style(String expression) {
+
+		Stack<BigDecimal> numbersAndOperators = new Stack<BigDecimal>();
+
+		Consumer<? super String> action = element -> {
+
+			switch (element) {
+			case "+":
+				applyOperation(numbersAndOperators, (n1 , n2 )-> n2.add(n1));
+				break;
+			case "-":
+				applyOperation(numbersAndOperators, (n1 , n2 )-> n2.subtract(n1));
+				break;
+			case "*":
+				applyOperation(numbersAndOperators, (n1 , n2 )-> n2.multiply(n1));
+				break;
+			case "/":
+				applyOperation(numbersAndOperators, (n1 , n2 )-> n2.divide(n1));
+				break;
+			default:
+				numbersAndOperators.push(new BigDecimal(element));
+			}
+		};
+		Arrays.stream(expression.split(SEPARATOR)).forEach(action);
+		;
+
+		return numbersAndOperators.pop();
+	}
+
+	private static void applyOperation(Stack<BigDecimal> numbersAndOperators, BiFunction<BigDecimal, BigDecimal, BigDecimal> operation) {
+		numbersAndOperators.push(operation.apply(numbersAndOperators.pop(), numbersAndOperators.pop()));
+		
+	}
+
 }
---
+++ src/test/java/rpn/ReversePolishNotationJava8Test.java
@@ -0,0 +1,123 @@
+package rpn;
+
+import static org.junit.Assert.*;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ReversePolishNotationJava8Test {
+
+	@Test
+	public void should_return_one_given_one_operand() {
+
+		String expression = "1";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(1));
+	}
+
+	@Test
+	public void should_return_one_given_one_operand_second_try() {
+
+		String expression = "4";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(4));
+	}
+
+	@Test
+	public void should_return_addition_given_2_numbers_and_one_plus() {
+
+		String expression = "4 1 +";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(5));
+	}
+
+	@Test
+	public void should_return_addition_given_2_numbers_and_one_plus_second_try() {
+
+		String expression = "14 1 +";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(15));
+	}
+	
+	@Test
+	public void should_return_multiplication_given_2_numbers_and_one_star() {
+
+		String expression = "10 2 *";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(20));
+	}
+	
+	@Test
+	public void should_return_substraction_given_2_numbers_and_one_minus() {
+
+		String expression = "10 2 -";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(8));
+	}
+	
+	@Test
+	public void should_return_division_given_2_numbers_and_one_slash() {
+
+		String expression = "10 2 /";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(5));
+	}
+	
+	@Test
+	public void should_return_result_given_two_operators() {
+
+		String expression = "4 2 + 3 -";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(3));
+	}
+
+	@Test
+	public void should_return_result_given_two_negative_operands() {
+
+		String expression = "-4 -2 +";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(-6));
+	}
+	
+	@Test
+	public void should_return_result_given_two_decimal_numbers() {
+
+		String expression = "4.6 2.8 +";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(7.4));
+	}
+	
+	@Test
+	public void should_return_result_given_two_operators_second_try() {
+
+		String expression = "4 2 - 7 +";
+		
+		BigDecimal result = ReversePolishNotation.computeUsingJava8Style(expression);
+		
+		assertThat(result).isEqualTo(BigDecimal.valueOf(9));
+	}
+	
+}
